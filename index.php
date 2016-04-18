8c3e5398473148f9b6ac7f5f41d989be3a6c021f
Leandro <lehnihon@hotmail.com>
2016-04-18 00:30:55 -0300

    endereço do maps

--- index.php
+++ index.php
@@ -94,8 +94,7 @@ get_header(); ?>
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-12 videoWrapper">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467814.9804093564!2d-47.041120950597595!3d-23.64717016917304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5855caa088ef%3A0xee3fef7424d63dfc!2sR.+S%C3%A3o+Bento%2C+545+-+Centro%2C+S%C3%A3o+Paulo+-+SP%2C+01011-100!5e0!3m2!1spt-BR!2sbr!4v1449080600451" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
-				</div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.0202897866575!2d-46.89584758435346!3d-23.603605269040322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cfaa060507c487%3A0x520f2d8917616578!2sEstr.+do+Capuava%2C+4421+-+Jardim+Ipes%2C+Cotia+-+SP!5e0!3m2!1spt-BR!2sbr!4v1460950196337" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>				</div>
 			</div>			
 		</div>
 	</section>
